485a1726e119398632f7730d024a9a1963c83fed
David Neubauer <davidneub@gmail.com>
2016-10-03 22:38:28 +0200

    Add cell classes

--- src/index.js
+++ src/index.js
@@ -1,22 +1,36 @@
 import createBoard from './board';
 import createMines from './mines';
 
-const rootNode = document.createElement('div');
-document.body.appendChild(rootNode);
-
 const boardSize = { x: 10, y: 10 };
-const amountMines = 4;
+const amountMines = 10;
 
 const generateRandom = (max) => Math.floor(Math.random() * (max + 1));
 
 const mines = createMines(boardSize, amountMines, generateRandom);
 const board = createBoard(boardSize, mines);
 
-board.forEach(row => {
+
+const rootNode = document.createElement('div');
+rootNode.className = 'app';
+document.body.appendChild(rootNode);
+
+const unveilCell = (cell, cellNode) => {
+  cellNode.className = 'cell-' + cell;
+  cellNode.textContent = cell;
+};
+
+const markCell = (e, cellNode) => {
+  e.preventDefault();
+  cellNode.className = 'cell-marked';
+};
+
+board.forEach((row, x) => {
   const rowNode = document.createElement('div');
-  row.forEach(cell => {
+  row.forEach((cell, y) => {
     const cellNode = document.createElement('button');
-    cellNode.textContent = cell;
+    cellNode.textContent = '?';
+    cellNode.addEventListener('click', () => unveilCell(board[x][y], cellNode));
+    cellNode.addEventListener('contextmenu', e => markCell(e, cellNode));
     rowNode.appendChild(cellNode);
   });
   rootNode.appendChild(rowNode);
