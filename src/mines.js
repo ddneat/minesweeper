38b4f09a6742e39ac1924219ecdbb6cc464637f0
David Neubauer <davidneub@gmail.com>
2016-10-03 22:38:14 +0200

    Add unique mine handling

--- src/mines.js
+++ src/mines.js
@@ -1,11 +1,20 @@
 export default (size, amount, generateRandom) => {
   const mines = [];
 
+  const isMineUnique = (mine) => {
+    const matchingMines = mines.filter(({ x, y }) => x === mine.x && y === mine.y);
+    return matchingMines.length === 0;
+  };
+
+  let candidate;
   while (mines.length < amount) {
-    mines.push({
-      x: generateRandom(size.x - 1),
-      y: generateRandom(size.y - 1)
-    });
+    do {
+      candidate = {
+        x: generateRandom(size.x - 1),
+        y: generateRandom(size.y - 1)
+      };
+    } while (!isMineUnique(candidate));
+    mines.push(candidate);
   }
 
   return mines;
--- src/mines.test.js
+++ src/mines.test.js
@@ -5,15 +5,15 @@ import createMines from './mines';
 suite('mines', () => {
   test('returns an array with the right amount of mines', () => {
     const size = { x: 2, y: 2 };
-    const amount = 2;
-    const generateRandom = () => {};
-    assert.equal(createMines(size, amount, generateRandom).length, 2);
+    const amount = 1;
+    const generateRandom = sinon.stub().returns(0);
+    assert.equal(createMines(size, amount, generateRandom).length, 1);
   });
 
   test('calls generateRandom with the amount of columns and rows', () => {
     const size = { x: 5, y: 2 };
-    const amount = 2;
-    const generateRandom = sinon.stub();
+    const amount = 1;
+    const generateRandom = sinon.stub().returns(0);
 
     createMines(size, amount, generateRandom);
     assert.deepEqual(generateRandom.getCall(0).args, [4]);
@@ -33,6 +33,25 @@ suite('mines', () => {
     assert.deepEqual(createMines(size, amount, generateRandom), [
       { x: 0, y: 2 },
       { x: 1, y: 5 }
+    ]);
+  });
+
+
+  test('does not return equal mines resulting form matching random coordinates', () => {
+    const size = { x: 2, y: 2 };
+    const amount = 2;
+    const generateRandom = sinon.stub();
+
+    generateRandom.onCall(0).returns(0);
+    generateRandom.onCall(1).returns(0);
+    generateRandom.onCall(2).returns(0);
+    generateRandom.onCall(3).returns(0);
+    generateRandom.onCall(4).returns(1);
+    generateRandom.onCall(5).returns(1);
+
+    assert.deepEqual(createMines(size, amount, generateRandom), [
+      { x: 0, y: 0 },
+      { x: 1, y: 1 }
     ]);
   });
 });
