4cde581a533a4f473f622d83aa69c6872f0d09b6
David Neubauer <davidneub@gmail.com>
2016-10-04 20:56:22 +0200

    Refactor cellNodes creation

--- src/index.js
+++ src/index.js
@@ -2,35 +2,43 @@ import createBoard from './board';
 import createMines from './mines';
 
 const boardSize = { x: 10, y: 10 };
-const amountMines = 10;
+const amountMines = 20;
 
-const generateRandom = (max) => Math.floor(Math.random() * (max + 1));
+const generateRandom = max => Math.floor(Math.random() * (max + 1));
 
 const mines = createMines(boardSize, amountMines, generateRandom);
 const board = createBoard(boardSize, mines);
 
-
+const cellNodes = {};
 const rootNode = document.createElement('div');
 rootNode.className = 'app';
 document.body.appendChild(rootNode);
 
-const unveilCell = (cell, cellNode) => {
-  cellNode.className = 'cell-' + cell;
-  cellNode.textContent = cell;
+const markCell = (e) => {
+  e.preventDefault();
+  e.target.setAttribute('class', 'cell-marked');
 };
 
-const markCell = (e, cellNode) => {
-  e.preventDefault();
-  cellNode.className = 'cell-marked';
+const unveilCell = (e) => {
+  const coordinates = e.target.getAttribute('cell');
+  const cellNode = cellNodes[coordinates];
+  const boardIndices = coordinates.split('|');
+  const cellValue = board[boardIndices[0]][boardIndices[1]];
+  cellNode.setAttribute('class', `cell-${cellValue}`);
+  cellNode.textContent = cellValue;
+  cellNode.removeEventListener('contextmenu', markCell);
 };
 
 board.forEach((row, x) => {
   const rowNode = document.createElement('div');
   row.forEach((cell, y) => {
     const cellNode = document.createElement('button');
+    const coordinates = `${x}|${y}`;
+    cellNode.setAttribute('cell', coordinates);
+    cellNodes[coordinates] = cellNode;
     cellNode.textContent = '?';
-    cellNode.addEventListener('click', () => unveilCell(board[x][y], cellNode));
-    cellNode.addEventListener('contextmenu', e => markCell(e, cellNode));
+    cellNode.addEventListener('click', unveilCell);
+    cellNode.addEventListener('contextmenu', markCell);
     rowNode.appendChild(cellNode);
   });
   rootNode.appendChild(rowNode);
