7335cd268cfa4317593c4c1aa4799ca5eb556d6a
David Neubauer <davidneub@gmail.com>
2016-10-06 19:58:14 +0200

    Add uncover algorithm

--- src/uncover.js
+++ src/uncover.js
@@ -1,13 +1,28 @@
 export default (board) => {
-  board.getCells();
+  const cells = board.getCells();
 
   return (x, y) => {
     if (!board.isInBoard(x, y)) {
       throw new Error('Coordinates not in board');
     }
 
-    const cellsToUncover = [{ x, y }];
-    board.getSurroundingCoordinates(x, y);
+    const cellsToUncover = [];
+
+    const isAlreadyInArray = coordinate => cellsToUncover.some(
+      cell => cell.x === coordinate.x && cell.y === coordinate.y
+    );
+
+    const uncoverCoordinate = (coordinate) => {
+      cellsToUncover.push({ x: coordinate.x, y: coordinate.y });
+
+      if (cells[coordinate.x][coordinate.y] === 0) {
+        board.getSurroundingCoordinates(coordinate.x, coordinate.y)
+          .filter(cell => board.isInBoard(cell.x, cell.y) && !isAlreadyInArray(cell))
+          .map(cell => uncoverCoordinate(cell));
+      }
+    };
+
+    uncoverCoordinate({ x, y });
 
     return cellsToUncover;
   };
--- src/uncover.test.js
+++ src/uncover.test.js
@@ -2,12 +2,15 @@ import assert from 'assert';
 import sinon from 'sinon';
 import createUncover from './uncover';
 
-suite.only('uncover', () => {
+suite('uncover', () => {
   let board;
 
   setup(() => {
+    const cells = [
+      [],
+    ];
     board = {
-      getCells: sinon.stub(),
+      getCells: sinon.stub().returns(cells),
       getSurroundingCoordinates: sinon.stub(),
       isInBoard: sinon.stub().returns(true),
     };
@@ -18,33 +21,58 @@ suite.only('uncover', () => {
     assert.ok(board.getCells.calledOnce);
   });
 
-  test('uncover calls board.isInBoard with the passed coordinates', () => {
+  test('calls board.isInBoard with the passed coordinates', () => {
     const uncover = createUncover(board);
-    const coordinates = { x: 1, y: 1 };
+    const coordinates = { x: 0, y: 0 };
     uncover(coordinates.x, coordinates.y);
     assert.ok(board.isInBoard.calledOnce);
     assert.deepEqual(board.isInBoard.lastCall.args, [coordinates.x, coordinates.y]);
   });
 
-  test('uncover throws when board.isInBoard returns false', () => {
+  test('throws when board.isInBoard returns false', () => {
     board.isInBoard.returns(false);
     const uncover = createUncover(board);
     assert.throws(() => uncover(), /Coordinates not in board/);
   });
 
-  test('uncover calls board.getSurroundingCoordinates with the passed coordinates', () => {
+  test('returns the an array of cells containing the cell with the passed coordinates', () => {
     const uncover = createUncover(board);
-    const coordinates = { x: 1, y: 1 };
+    const coordinates = { x: 0, y: 0 };
+    assert.deepEqual(uncover(coordinates.x, coordinates.y), [
+      { x: coordinates.x, y: coordinates.y },
+    ]);
+  });
+
+  test('calls board.getSurroundingCoordinates when the target cell has the value 0', () => {
+    board.getCells.returns([[0], [0]]);
+    board.getSurroundingCoordinates.returns([]);
+    const uncover = createUncover(board);
+    const coordinates = { x: 0, y: 0 };
     uncover(coordinates.x, coordinates.y);
     assert.ok(board.getSurroundingCoordinates.calledOnce);
     assert.deepEqual(board.getSurroundingCoordinates.lastCall.args, [coordinates.x, coordinates.y]);
   });
 
-  test('returns the an array of cells', () => {
+  test('does not call board.getSurroundingCoordinates when the target cell has the value 1', () => {
+    board.getCells.returns([[0], [1]]);
     const uncover = createUncover(board);
-    const coordinates = { x: 1, y: 1 };
+    const coordinates = { x: 0, y: 1 };
+    uncover(coordinates.x, coordinates.y);
+    assert.equal(board.getSurroundingCoordinates.callCount, false);
+  });
+
+  test('returns an array of cells containing all surrounding 0 cells and their surroundings', () => {
+    board.getCells.returns([[0], [1]]);
+    board.isInBoard.onCall(3).returns(false);
+    board.getSurroundingCoordinates.onCall(0).returns([{ x: 1, y: 0 }]);
+    board.getSurroundingCoordinates.onCall(1).returns([{ x: 1, y: 1 }]);
+    board.getSurroundingCoordinates.returns([]);
+    const uncover = createUncover(board);
+    const coordinates = { x: 0, y: 0 };
+
     assert.deepEqual(uncover(coordinates.x, coordinates.y), [
       { x: coordinates.x, y: coordinates.y },
+      { x: 1, y: 0 },
     ]);
   });
 });
