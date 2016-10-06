788c3ca8166097931e58581c74306414b83902a2
David Neubauer <davidneub@gmail.com>
2016-10-06 20:00:18 +0200

    Update view handling

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import createBoard from './board';
 import createMines from './mines';
+import createUncover from './uncover';
 
-const boardSize = { x: 10, y: 10 };
-const amountMines = 20;
+const boardSize = { x: 20, y: 20 };
+const amountMines = 70;
 
 const generateRandom = max => Math.floor(Math.random() * (max + 1));
 
 const mines = createMines(boardSize, amountMines, generateRandom);
 const board = createBoard(boardSize, mines);
+const uncover = createUncover(board);
 const cells = board.getCells();
 
 const cellNodes = {};
@@ -15,19 +17,39 @@ const rootNode = document.createElement('div');
 rootNode.className = 'app';
 document.body.appendChild(rootNode);
 
+const scoreNode = document.createElement('div');
+scoreNode.className = 'score';
+rootNode.appendChild(scoreNode);
+
+let markedCount = 0;
+scoreNode.textContent = markedCount;
+
 const markCell = (e) => {
   e.preventDefault();
-  e.target.setAttribute('class', 'cell-marked');
+  if (e.target.className === 'cell-marked') {
+    markedCount -= 1;
+    scoreNode.textContent = markedCount;
+    e.target.setAttribute('class', '');
+  } else if (e.target.className === '') {
+    markedCount += 1;
+    scoreNode.textContent = markedCount;
+    e.target.setAttribute('class', 'cell-marked');
+  }
 };
 
 const unveilCell = (e) => {
+  if (e.target.className === 'cell-marked') return;
   const coordinates = e.target.getAttribute('cell');
-  const cellNode = cellNodes[coordinates];
   const cellIndices = coordinates.split('|');
-  const cellValue = cells[cellIndices[0]][cellIndices[1]];
-  cellNode.setAttribute('class', `cell-${cellValue}`);
-  cellNode.textContent = cellValue;
-  cellNode.removeEventListener('contextmenu', markCell);
+  const cellsToUncover = uncover(parseInt(cellIndices[0], 10), parseInt(cellIndices[1], 10));
+  cellsToUncover.forEach((cell) => {
+    const cellValue = cells[cell.x][cell.y];
+    const cellNode = cellNodes[`${cell.x}|${cell.y}`];
+    if (cellNode.className === 'cell-marked') return;
+    const cellType = cellValue === 'm' ? 'mine' : cellValue;
+    cellNode.setAttribute('class', `cell-${cellType}`);
+    cellNode.textContent = cellValue;
+  });
 };
 
 cells.forEach((row, x) => {
@@ -35,8 +57,8 @@ cells.forEach((row, x) => {
   row.forEach((cell, y) => {
     const cellNode = document.createElement('button');
     const coordinates = `${x}|${y}`;
-    cellNode.setAttribute('cell', coordinates);
     cellNodes[coordinates] = cellNode;
+    cellNode.setAttribute('cell', coordinates);
     cellNode.textContent = '?';
     cellNode.addEventListener('click', unveilCell);
     cellNode.addEventListener('contextmenu', markCell);
